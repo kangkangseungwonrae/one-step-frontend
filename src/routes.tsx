dec528f19411b702561cd7e1a9cd50fadf53cc31
taboowiths <tabou9820@gmail.com>
2025-11-29 18:02:18 +0900

    feat: onboarding 페이지 생성 및 기본 기능 구현

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router';
 
 import Main from './pages/main';
+import Onboarding from './pages/onboarding';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: Main,
+  },
+  {
+    path: '/onboarding',
+    Component: Onboarding,
   },
 ]);
