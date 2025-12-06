f9c01add4702f43cf9dc211f4be4c349057ad82e
taboowiths <tabou9820@gmail.com>
2025-12-07 01:11:19 +0900

    feat: 로그인 페이지 UI 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import GoogleIcon from '@/assets/icons/google.svg?react';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function LoginPage() {
   const handleGoogleLogin = () => {
@@ -7,11 +8,27 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-      <Button onClick={handleGoogleLogin} variant="outline">
-        <GoogleIcon />
-        <span>구글 로그인</span>
-      </Button>
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <Card className="flex items-center text-center gap-6 w-full max-w-md">
+        <CardHeader className="pt-2 pb-0 w-full">
+          <CardTitle className="text-3xl font-black tracking-tight text-card-foreground">One Step</CardTitle>
+          <p className="mt-2 text-sm text-muted-foreground">하루에 단 하나만, 지금 할 수 있는 것부터 시작해봐요.</p>
+        </CardHeader>
+        <CardContent className="w-full flex flex-col items-center gap-6 pt-2 px-6">
+          <Button
+            onClick={handleGoogleLogin}
+            variant="outline"
+            className="w-full flex items-center justify-center gap-2"
+          >
+            <GoogleIcon />
+            <span className="font-medium">구글 계정으로 시작하기</span>
+          </Button>
+          <div className="flex flex-col items-center *:text-xs *:text-muted-foreground">
+            <p>구글 로그인 버튼을 누르면 서비스 이용 약관과</p>
+            <p>개인정보 처리방침에 동의하는 것으로 간주됩니다.</p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
